ef87cd4e809af82b91fae139a9fb562ef9f254c4
Svaught598 <svaught598@gmail.com>
2020-01-19 13:29:05 -0600

    fixed mandelbrot set

--- Python Programming/Homework 01/Newman_3.7.py
+++ Python Programming/Homework 01/Newman_3.7.py
@@ -1,24 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-def mandelbrot(x, y):
-    c = np.complex(x, y)
-    z = 0
+from pprint import pprint
+
+# Complex Array initialization
+temp = np.linspace(-2, 2, 1000)
+x = np.vstack([temp, temp])
+for i in range(998):
+    x = np.vstack([x, temp])
+complex_numbers = x + np.transpose(x) * 1J
+
+
+def mandelbrot(complex_array):
+    # local function variables
+    z = np.zeros((1000, 1000))
+    mandelbrot_set = np.zeros((1000, 1000))
+
+    # Begin loop for Mandbrot set
     for i in range(500):
-        z = z**2 + c
-        if abs(z) > 2:
-            return 1
-
-xi = np.arange(-2, 2, 1000)
-yi = np.arange(-2, 2, 1000)
+        z = np.square(z) + complex_array
+        mandelbrot_set[np.abs(z) > 2] = i
+     
+    return mandelbrot_set
 
 
-pixels = np.empty([1000,1000], int)
-for i, x in enumerate(xi):
-    for k, y in enumerate(yi):
-        pixels[i, k] = mandelbrot(x, y)
-
-plt.imshow(pixels, origin = "lower")
-plt.winter()
+plt.imshow(
+    mandelbrot(complex_numbers),
+)
 plt.show()
-
