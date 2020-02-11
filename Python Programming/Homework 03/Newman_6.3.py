6a4ec8a295009f451ef2342a751979da475819e0
Svaught598 <svaught598@gmail.com>
2020-02-11 11:57:17 -0600

    created blank problem files

--- Python Programming/Homework 03/Newman_6.3.py
+++ Python Programming/Homework 03/Newman_6.3.py
@@ -12,28 +12,30 @@ def lu_decomposition(array):
     N = len(array)
     L = np.zeros([N, N], float)
     U = np.zeros([N, N], float)
-    U[:] = array
-
+    # U[:] = array
     
+    # loop through rows
     for i in range(N):
         L[i, i] = 1.0
-        for j in range(N):
-            U[i, j] = array[i, j] - sum(U[k, j]*L[i, k] for k in range(i))
 
+        # loop through columns for U
         for j in range(i, N):
-            L[i, j] = array[i, j] - sum(U[k, j]*L[i, k] for k in range(j))
-            L[i, j] /= U[j, j]
+            U[i, j] = array[i, j] - sum(L[i, k]*U[k, j] for k in range(i))
 
+        # loop through columns for L
+        for j in range(i):
+            L[i, j] = array[i, j] - sum(L[i, k]*U[k, j] for k in range(j))
+            L[i, j] /= U[j, j]
 
     return L, U
 
 
 
 A = np.array(
-    [[2, 1, 4, 1],
-    [3, 4, -1, -1],
-    [1, -4, 1, 5],
-    [2, -2, 1, 3]],
+    [[2,  1,  4,  1],
+    [ 3,  4, -1, -1],
+    [ 1, -4,  1,  5],
+    [ 2, -2,  1,  3]],
     float)
 V = np.array([-4, 3, 9, 7], float)
 l, u = lu_decomposition(A)
@@ -44,19 +46,22 @@ print(f'Are Equal? \n{np.allclose(A, np.dot(l, u))}\n')
 
 
 """ Part B ++++++++++++++++++++++++++++++++++++++++++++++++"""
-N = len(A)
 
-y = np.empty(N, float)
-for i in range(N-1, -1, -1):
-    y[i] = V[i]
-    for j in range(i+1, N):
-        y[i] -= A[i,j]*V[j]
+# N = len(A)
 
-x = np.empty(N, float)
-for i in range(N):
-    y[i] = V[i]
-    for j in range(i+1, N):
-        y[i] -= A[i,j]*V[j]
+# y = np.empty(N, float)
+# for i in range(N-1, -1, -1):
+#     y[i] = V[i]
+#     for j in range(N-1, i, -1):
+#         y[i] -= u[i,j]*V[j]
+#         print(u[i,j])
 
-print(x)
-print(np.linalg.solve(A,V))
+# x = np.empty(N, float)
+# for i in range(N):
+#     x[i] = y[i]
+#     for j in range(i+1, N):
+#         x[i] -= l[i,j]*y[j]
+
+# print(x)
+# print(np.linalg.solve(A,V))
+
